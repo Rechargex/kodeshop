1d45cd7528e784dc10459e004005d5f417acab06
Rechargex <syhntax@gmail.com>
2023-02-08 13:52:21 +0800

    Use Call Back

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [url, setUrl] = useState("http://localhost:8000/products");
 
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+  const fetchProducts = useCallback(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setProducts(data);
   }, [url]);
+
+  useEffect(() => {
+    fetchProducts();
+    console.log("-");
+  }, [fetchProducts]);
 
   return (
     <section>
